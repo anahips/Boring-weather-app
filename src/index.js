784eed5a888862f0bc603f953450523b059047d5
anahips <102506256+anahips@users.noreply.github.com>
2022-04-24 21:20:51 +0100

    added some js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let weatherDescription = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "49377281846eee971852e0a6a46bc4a0";
